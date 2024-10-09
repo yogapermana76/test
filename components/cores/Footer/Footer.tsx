cb9325ffeb65af5a65c6a97a8bb41f98b586c081
Yoga Permana <permana76.yp@gmail.com>
2024-10-09 09:41:41 +0700

    fix: footer - add forwardRef

--- components/cores/Footer/Footer.tsx
+++ components/cores/Footer/Footer.tsx
@@ -1,13 +1,19 @@
+import React from 'react';
 import { Box, BoxProps } from '../Container';
 import Reanimated from 'react-native-reanimated';
 
 export interface FooterProps extends BoxProps {}
 
-export const Footer = function Footer(props: FooterProps) {
+// Gunakan React.forwardRef untuk mendukung ref forwarding
+export const Footer = React.forwardRef(function Footer(
+  props: FooterProps,
+  ref
+) {
   const { children, bottom = 0, withSafeArea = 'bottomOnly', ...rest } = props;
 
   return (
     <Box
+      ref={ref} // Pastikan ref diteruskan ke komponen yang memerlukan ref
       bottom={bottom}
       left={0}
       right={0}
@@ -17,6 +23,7 @@ export const Footer = function Footer(props: FooterProps) {
       {children}
     </Box>
   );
-};
+});
 
+// Bungkus Footer dengan Reanimated.createAnimatedComponent
 export const ReanimatedFooter = Reanimated.createAnimatedComponent(Footer);
