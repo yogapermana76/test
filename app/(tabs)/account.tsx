36d4c038299b1c47de339abf83ac24a8ee38ae3e
Yoga Permana <permana76.yp@gmail.com>
2024-10-14 14:27:49 +0700

    feat: tabs - init tab pages

---
+++ app/(tabs)/account.tsx
@@ -0,0 +1,5 @@
+import { Text } from '@/components/cores';
+
+export default function Account() {
+  return <Text text="Account" />;
+}
---
+++ app/(tabs)/notification.tsx
@@ -0,0 +1,5 @@
+import { Text } from '@/components/cores';
+
+export default function Notification() {
+  return <Text text="Notification" />;
+}
